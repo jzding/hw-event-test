cd2a255a4927fc65455a6acab7701fc1c9d163be
Jack Ding <jacding@redhat.com>
2022-01-21 12:18:14 -0500

    update pem dir
    Signed-off-by: Jack Ding <jacding@redhat.com>

--- webhook/myserver.py
+++ webhook/myserver.py
@@ -1,7 +1,10 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import ssl
+import sys
 from io import BytesIO
+from pathlib import Path
 
+KEY_DIR = Path.cwd() / '.pem'
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
@@ -12,6 +15,7 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
+        print(body)
         self.send_response(200)
         self.end_headers()
         response = BytesIO()
@@ -20,12 +24,16 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         response.write(body)
         self.wfile.write(response.getvalue())
 
+if len(sys.argv) < 2:
+    print("usage: {} <ip address of the local host>".format(sys.argv[0]))
+
+host_ip = sys.argv[1]
 # run this on cnfdd4 10.19.17.161
-httpd = HTTPServer(('10.19.17.161', 4443), MyHTTPRequestHandler)
+httpd = HTTPServer((host_ip, 4443), MyHTTPRequestHandler)
 
 # copy *.pem to current directory
 httpd.socket = ssl.wrap_socket (httpd.socket, 
-        keyfile="key.pem", 
-        certfile='cert.pem', server_side=True)
+        keyfile=KEY_DIR / "key.pem", 
+        certfile=KEY_DIR / 'cert.pem', server_side=True)
 
 httpd.serve_forever()
